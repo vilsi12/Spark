e6fe8085ecb250f95c911b2ec796a105ccc1de55
Ayush Kejariwal <kejayush29@gmail.com>
2021-05-21 00:34:54 +0530

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:chatapp/views/chatrooms.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -14,7 +15,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   bool userIsLoggedIn;
 
   @override
@@ -24,9 +24,9 @@ class _MyAppState extends State<MyApp> {
   }
 
   getLoggedInState() async {
-    await HelperFunctions.getUserLoggedInSharedPreference().then((value){
+    await HelperFunctions.getUserLoggedInSharedPreference().then((value) {
       setState(() {
-        userIsLoggedIn  = value;
+        userIsLoggedIn = value;
       });
     });
   }
@@ -43,12 +43,15 @@ class _MyAppState extends State<MyApp> {
         fontFamily: "OverpassRegular",
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: userIsLoggedIn != null ?  userIsLoggedIn ? ChatRoom() : Authenticate()
+      home: userIsLoggedIn != null
+          ? userIsLoggedIn
+              ? ChatRoom()
+              : Authenticate()
           : Container(
-        child: Center(
-          child: Authenticate(),
-        ),
-      ),
+              child: Center(
+                child: Authenticate(),
+              ),
+            ),
     );
   }
 }
